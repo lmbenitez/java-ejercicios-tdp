39867065f09829d710f3e5005bc05ef04dfcef60
Rhaynel Parra Aguiar <me@rhynl.io>
2017-08-13 20:32:46 -0300

    ejercicio11 branch

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Ejercicios sobre tecnicas de programación (TDP) en Java
 
-Los ejercicios estan separados en diferentes ramas del repositorio, la idea es que cada estudiante descargue el repositorio, elija uno de los ejercicios y lo resuelva.
+## EJERCICIO 11: Programa java que realice lo siguiente
+
+* Lea una cantidad de grados centígrados y la pase a grados Fahrenheit. La fórmula correspondiente es: F = 32 + ( 9 * C / 5)
+
+## SPOILER ALERT
+
+Posible solución [aquí](http://puntocomnoesunlenguaje.blogspot.com.es/2012/06/java-ejercicios-basicos-2.html).
--- src/cac/java/tdp/Main.java
+++ src/cac/java/tdp/Main.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Ejercicio 11: Programa que
+ * Lea una cantidad de grados centígrados y la pase a grados Fahrenheit.
+ * La fórmula correspondiente es: F = 32 + ( 9 * C / 5)
  */
 package cac.java.tdp;
 
@@ -15,7 +15,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!!");
+        
     }
     
 }
