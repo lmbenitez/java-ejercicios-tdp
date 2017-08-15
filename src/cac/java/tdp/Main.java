22136f8ec3d7dd8b3655cc230a83e06565af2d1f
Luis Miguel Benitez <lm.benitez@hotmail.com>
2017-08-15 20:20:33 -0300

    calculadura que convierte de Km/h a m/s

--- src/cac/java/tdp/Main.java
+++ src/cac/java/tdp/Main.java
@@ -4,6 +4,9 @@
  */
 package cac.java.tdp;
 
+import javax.swing.JOptionPane;
+
+
 /**
  *
  * @author Rhaynel Parra Aguiar <mail@rhynl.io>
@@ -14,6 +17,17 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        
+        String km = JOptionPane.showInputDialog("Instroduzca los Km/h");
+        
+        int kmH = Integer.parseInt(km);
+        
+        double mS;
+        
+        mS=(double)kmH/3.6;
+        
+        JOptionPane.showMessageDialog(null,"El resultado en m/s es: "+ mS);
         
     }
     
