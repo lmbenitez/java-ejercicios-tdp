7233673a1622a1cc2314c15c003470480ced1169
Luis Miguel Benitez <lm.benitez@hotmail.com>
2017-08-15 21:11:16 -0300

    calculadora de area y longitud de circunferencia en base al area

--- src/cac/java/tdp/Main.java
+++ src/cac/java/tdp/Main.java
@@ -6,6 +6,8 @@
  */
 package cac.java.tdp;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Rhaynel Parra Aguiar <mail@rhynl.io>
@@ -16,6 +18,23 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        String ra = JOptionPane.showInputDialog("Instroduzca el radio en milimetros");
+        
+        int  radio = Integer.parseInt(ra);
+        
+        double longitud;
+        
+        double area;
+        
+        double PI = 3.1416;
+        
+        longitud=(double)2*PI*radio;
+        
+        area = (double)(PI*radio)*(PI*radio);
+        
+        JOptionPane.showMessageDialog(null,"La longitud de la circunferencia en milimetro es: "+ longitud
+        +"\n El area de la circunferencia en milimetros es: "+area);
         
     }
     
